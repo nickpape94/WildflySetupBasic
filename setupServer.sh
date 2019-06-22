86bed2053cf0ddc265498d55c46d71548b939cb9
lucy <50924589+lushilling@users.noreply.github.com>
2019-06-22 12:01:11 +0100

    Update setupServer.sh

--- setupServer.sh
+++ setupServer.sh
@@ -4,9 +4,9 @@ sudo apt-get install unzip
 sudo apt-get update
 sudo apt-get install maven -y
 sudo apt install -y openjdk-8-jdk
+cd /var/lib
 wget "https://download.jboss.org/wildfly/10.1.0.Final/wildfly-10.1.0.Final.zip"
 unzip wildfly-10.1.0.Final.zip
 rm wildfly-10.1.0.Final/standalone/configuration/standalone.xml
-cp WildflySetupBasic/standalone.xml wildfly-10.1.0.Final/standalone/configuration/
+cp ~/WildflySetupBasic/standalone.xml wildfly-10.1.0.Final/standalone/configuration/
 sudo sh wildfly-10.1.0.Final/bin/standalone.sh &
-
